e784f3870de783e518c7b31d0c687de6912b2875
Dan Burbach and Torin Huff <torin@>
2018-09-12 20:55:16 -0700

    changed everything to allow for it to work

--- js/scripts.js
+++ js/scripts.js
@@ -15,34 +15,37 @@ var divide = function(number1, number2) {
 };
 
 $(document).ready(function() {
-  $("form#calc").submit(function(event) {
+  $("form#calc1").submit(function(event) {
     event.preventDefault();
-    var number1 = parseInt($("#calc1").val());
-    var number2 = parseInt($("#calc2").val());
+    var number1 = parseInt($("#addition1").val());
+    var number2 = parseInt($("#addition2").val());
     var result = add(number1, number2);
     debugger;
     $("#output").text(result);
   });
 
-  $("form#calc").submit(function(event) {
-    var number1 = parseInt($("#calc1").val());
-    var number2 = parseInt($("#calc2").val());
+  $("form#calc2").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#subtraction1").val());
+    var number2 = parseInt($("#subtraction2").val());
     var result = subtract(number1, number2);
     debugger;
     $("#output").text(result);
   });
 
-  $("form#calc").submit(function(event) {
-    var number1 = parseInt($("#calc1").val());
-    var number2 = parseInt($("#calc2").val());
+  $("form#calc3").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#multiplication1").val());
+    var number2 = parseInt($("#multiplication2").val());
     var result = multiply(number1, number2);
     debugger;
     $("#output").text(result);
   });
 
-  $("form#calc").submit(function(event) {
-    var number1 = parseInt($("#calc1").val());
-    var number2 = parseInt($("#calc2").val());
+  $("form#calc4").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#division1").val());
+    var number2 = parseInt($("#division2").val());
     var result = divide(number1, number2);
     debugger;
     $("#output").text(result);
