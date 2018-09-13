04bf26666cf4ef00340cdb36f2af09a0173db774
Dan Burbach and Torin Huff <dan@>
2018-09-12 18:57:49 -0700

    updated scripts.js file to contain working code

--- js/scripts.js
+++ js/scripts.js
@@ -13,3 +13,10 @@ var multiply = function(number1, number2) {
 var divide = function(number1, number2) {
   return number1 / number2;
 };
+
+$(document).ready(function() {
+  var number1 = parseInt(prompt("Enter a number:"));
+  var number2 = parseInt(prompt("Enter another number:"));
+
+  alert(add(number1, number2));
+});
